5ebd98b24b3b120fdae54f9cee1a4fc0eb8e08ea
Srinivas G <srinivasg.mca002@gmail.com>
2025-09-16 16:29:31 +0530

    Read More example added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
-import ChipsInput from './components/coding-challenges/ChipsInput/ChipInput';
-import { ConfirmationModal } from './components/coding-challenges/confirmation-modal/ConfirmationModal';
+//import ChipsInput from './components/coding-challenges/ChipsInput/ChipInput';
+//import { ConfirmationModal } from './components/coding-challenges/confirmation-modal/ConfirmationModal';
+import ReadMoreToggle from './components/coding-challenges/ReadMoreToggle.js/ReadMoreToggle';
 
 function App() {
   return (
     <div className="App">
       {/* <ChipsInput /> */}
-      <ConfirmationModal />
+      {/* <ConfirmationModal /> */}
+      <ReadMoreToggle />
     </div>
   );
 }
