6f943b02ebf1b48da0d1c3e0dbbce8722684ad37
Srinivas G <srinivasg.mca002@gmail.com>
2025-09-24 12:52:39 +0530

    lazy loading and suspense example added with routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
+
 //import ChipsInput from './components/coding-challenges/ChipsInput/ChipInput';
 //import { ConfirmationModal } from './components/coding-challenges/confirmation-modal/ConfirmationModal';
 import ReadMoreToggle from './components/coding-challenges/ReadMoreToggle.js/ReadMoreToggle';
@@ -15,6 +17,10 @@ import ToggleButton from "./components/core-concepts/custom-hooks/useToggle";
 import CounterComponent from "./components/core-concepts/custom-hooks/useCounter";
 import PostList from "./components/core-concepts/custom-hooks/useFetch";
 import ParentComponent from "./components/performance-techniques/react-memo-pure-components";
+
+const Home = lazy(()=>import('./components/performance-techniques/lazy-loading-with-routes/Home'));
+const About = lazy(()=>import('./components/performance-techniques/lazy-loading-with-routes/About'));
+const Contact = lazy(()=>import('./components/performance-techniques/lazy-loading-with-routes/Contact'));
 
 function App() {
   // const fruits = ['apple', 'banana', 'orange', 'guava'];
@@ -39,7 +45,19 @@ function App() {
       {/* <ToggleButton /> */}
       {/* <CounterComponent /> */}
       {/* <PostList /> */}
-      <ParentComponent />
+      {/* <ParentComponent /> */}
+
+      {/* Routing */}
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/performance-techniques/lazy-loading-with-routes/About.js
@@ -0,0 +1,20 @@
+
+/*
+Lazy loading is a technique where components are loaded only when needed, rather than at the initial page load. In React, React.lazy() enables this by allowing you to dynamically import components.
+Definition: Lazy loading is the process of deferring the loading of a component until it is required, improving performance and reducing initial bundle size.
+
+
+Suspense
+Suspense is a React component that lets you handle the loading state of lazy-loaded components. It shows a fallback UI (like a spinner or message) while the component is being fetched.
+Definition: Suspense is a React feature used to display a fallback UI while waiting for a lazy-loaded component or asynchronous data to load.
+
+*/
+
+
+const About = () =>{
+    return(
+        <h2>About Page</h2>
+    )
+}
+
+export default About;
